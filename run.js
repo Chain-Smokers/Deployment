994f640870e2a6c612f70c8c28b337bc0ab636b8
Yash Eksambekar <yasheksambekar@gmail.com>
2022-12-07 11:43:41 +0530

    Seperated init and run scripts

---
+++ run.js
@@ -0,0 +1,49 @@
+const { spawn, exec, execSync } = require("child_process");
+const fs = require("fs");
+const config = require("./config.json");
+
+const bootnodeDir = `./${config.networkName}/${config.bootnodeName}`;
+const accounts = require("./accounts.json");
+const genesis = require(`./${config.networkName}/genesis.json`)
+const nodesDir = Array.from(Array(config.numberOfSealers).keys()).map(
+    (nodeNum) => `./${config.networkName}/${config.sealerRootName}${nodeNum + 1}`
+);
+
+const bootnode = spawn("bootnode", ["-nodekey", `${bootnodeDir}/boot.key`, "-addr", ":30305"])
+let ethstatsJson = []
+bootnode.stdout.on('data', (data) => {
+    console.log(data.toString())
+    if (data.toString().startsWith("enode://")) {
+        execSync("pm2 kill")
+        for (let i = 0; i < accounts.length; i++) {
+            exec(`geth --datadir ${nodesDir[i]} --port ${30306 + i} --authrpc.port ${8545 + config.numberOfSealers + i} --http.port ${8545 + i} --bootnodes ${data.toString().split("\n")[0]} --networkid ${genesis.config.chainId} --unlock 0x${accounts[i].address} --password password.txt --syncmode full --allow-insecure-unlock --http --http.corsdomain "*" --mine`)
+            ethstatsJson.push({
+                "name": `${config.sealerRootName}${i + 1}`,
+                "script": "app.js",
+                "log_date_format": "YYYY-MM-DD HH:mm Z",
+                "merge_logs": false,
+                "watch": false,
+                "max_restarts": 10,
+                "exec_interpreter": "node",
+                "exec_mode": "fork_mode",
+                "env":
+                {
+                    "NODE_ENV": "production",
+                    "RPC_HOST": "localhost",
+                    "RPC_PORT": `${8545 + i}`,
+                    "LISTENING_PORT": `${30306 + i}`,
+                    "INSTANCE_NAME": `${config.sealerRootName}${i + 1}`,
+                    "CONTACT_DETAILS": "",
+                    "WS_SERVER": "http://localhost:3000",
+                    "WS_SECRET": `${config.WS_SECRET}`,
+                    "VERBOSITY": 2
+                }
+            })
+        }
+        fs.writeFileSync(`eth-net-intelligence-api/app.json`, JSON.stringify(ethstatsJson));
+        exec("cd eth-net-intelligence-api && pm2 start app.json")
+        exec(`cd eth-netstats && PORT=3000 WS_SECRET="${config.WS_SECRET}" npm start`)
+        exec("cd explorer && npm start")
+        console.log("\nDone!")
+    }
+})
